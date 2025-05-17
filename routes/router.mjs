ff54e2d8408fdaf91b77484c7c59f36725878c15
Giorgos Konstantouras <68753823+giorgoskon3@users.noreply.github.com>
2025-05-17 20:45:54 +0300

    fix

--- routes/router.mjs
+++ routes/router.mjs
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get('/', controller.showHome);
 router.get('/job-seeker', controller.showJobSeeker);
 router.get('/job-seeker/jobSearch', controller.showJobSearch);
-router.get('/job-seeker/jobSearch/results', controller.searchJobs);
+// router.get('/job-seeker/jobSearch/results', controller.searchJobs);
 // router.get('/job-seeker/savedJobs', controller.showSavedJobs);
 
 router.get('/employer', controller.showEmployer);
